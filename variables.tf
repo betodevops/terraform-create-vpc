d1a0d518851d5a80fbbc6dfd9bc1ed1542bfe244
System Administrator <root@1080028teste.local>
2019-06-13 11:09:29 -0300

    adicionando novos arquivos

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  description = "Region for the VPC"
+  default = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the private subnet"
+  default = "10.0.2.0/24"
+}
+
